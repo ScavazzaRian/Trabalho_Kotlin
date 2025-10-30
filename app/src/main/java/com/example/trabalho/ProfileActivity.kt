7775584d17698a4846cbdb0f8e6ba70ac597edbc
Juliano Santos <julianosantosdev@outlook.com>
2025-10-30 12:54:32 -0300

    feat: nav buttons intent

--- app/src/main/java/com/example/trabalho/ProfileActivity.kt
+++ app/src/main/java/com/example/trabalho/ProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.example.trabalho
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +22,14 @@ class ProfileActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val homeButton: View = findViewById(R.id.nav_home)
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
